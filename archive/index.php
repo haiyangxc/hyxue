2f3587dbd2e9040100cf496e8fb5b97f72caf3d6
Xue Haiyang <67530474+haiyangxc@users.noreply.github.com>
2020-09-15 23:50:31 +0800

    1

--- archive/index.php
+++ archive/index.php
@@ -1,22 +1,15 @@
 <!--
  * @Author: Jon Barron
  * @Date: 2019-05-11 22:15:22
- * @LastEditors: whyisyoung
- * @LastEditTime: 2020-06-28 15:06:46
+ * @LastEditors: haiyang xue
+ * @LastEditTime: 2020-09-15
  -->
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
 
 <head>
-  <!-- Global site tag (gtag.js) - Google Analytics -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-45384067-2"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag() { dataLayer.push(arguments); }
-    gtag('js', new Date());
 
-    gtag('config', 'UA-45384067-2');
-  </script>
+
 
   <!-- <meta name=viewport content="width=800"> -->
   <meta name=viewport content="width=device-width">
@@ -105,8 +98,8 @@
     }
 
   </style>
-  <link rel="icon" type="image/png" href="images/limin-icon.jpg">
-  <title>Limin Yang</title>
+  <link rel="icon" type="image/png" href="images/x.jpg">
+  <title>Haiyang Xue</title>
   <meta http-equiv="Content-Type" content="text/html; charset=us-ascii">
   <link href='https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
 </head>
@@ -123,64 +116,27 @@
           </colgroup>
           <tr>
             <td valign="middle">
-              <img src="images/liminyang.jpg" height="170px">
-              <p class="footer" style="font-size: 15px;">photo credit: <a href="https://www.haileyliu.photos/" target="_blank" style="font-size: 15px"> Hailey Liu</a>  </p>
+              <img src="images/haiyang.jpg" height="170px">
             </td>
             <td valign="middle">
               <p align="left">
-                <name>Limin Yang &nbsp; (杨利民) </name>
+                <name>Haiyang Xue &nbsp; (薛海洋) </name>
               </p>
-              <p>I am a Ph.D. student of Computer Science at <a href="https://illinois.edu">UIUC</a> since 08/2019. <br>
-                 I focus on <em class="highlight">security, measurement, and explainable AI. </em> <br>
-                 My advisor is Dr. <a href="https://gangw.web.illinois.edu/">Gang Wang</a>.
+              <p>I am a research associate at <a href="http://www.iie.ac.cn/">IIE, CAS </a> since 07/2015. <br>
+			     Currently, I am a postdoc researcher at <a href="https://www.hku.hk/"> the University of Hong Kong </a> with Prof. <a href="https://gangw.web.illinois.edu/"> Man Ho Au </a> <br>
+                 My research interest includes post-quantum cryptography, authenticated key exchange, and zero-knowledge proof. </em> <br>
               </p>
 
-              <p align=left>Contact: liminy2@illinois.edu</p>
-              <p align=left>Office: Room 4111, Thomas M. Siebel Center, Urbana, IL 61801 </p>
+              <p align=left>Contact: haiyangxc@gmail.com</p>
               <p align=left>
-                <!-- <a href="data/Resume_LiminYang.pdf">Resume</a> &nbsp;/&nbsp; -->
-                <a href="data/CV_LiminYang_UIUC.pdf">CV</a> &nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;
-                <a href="https://github.com/whyisyoung"> GitHub </a> &nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;
-                <a href="https://scholar.google.com/citations?hl=en&user=FCNYLcUAAAAJ">Google Scholar</a> &nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;
-                <a href="https://www.linkedin.com/in/liminyang/"> LinkedIn </a>
+                <a href="https://scholar.google.com/citations?user=C5kU67QAAAAJ">Google Scholar</a> 
               </p>
             </td>
           </tr>
         </table>
         <!-- Intro End -->
 
-        <!-- News Begin -->
-        <table width="100%" align="center" border="0" cellspacing="0" cellpadding="12">
-          <tr>
-            <td>
-              <heading>News</heading>
-            </td>
-          </tr>
-        </table>
-        <table width="100%" align="center" border="0" cellpadding="12" style="table-layout:fixed;">
-          <colgroup>
-            <col span="1" style="width: 23%;">
-            <col span="1" style="width: 77%;">
-          </colgroup>
-          <tr>
-            <td valign="center">
-              08/01/2020
-            </td>
-            <td valign="center" style="color:#E9623F">
-                  <strong>I'm actively looking for a security and deep learning internship for 2021 summer!</strong>
-            </td>
-          <tr>
-            <td valign="center">
-              06/18/2020
-            </td>
-            <td valign="center">
-                  Submitted my first paper to USENIX Security 2021! <br>
-                  Thanks a lot to my advisor and collaborators!
-                  <!-- <li><strong>07/2019</strong>: &nbsp; Our VirusTotal reliability paper got accepted to IMC 2019.</li> -->
-            </td>
-          </tr>
-        </table>
-        <!-- News End -->
+
 
         <!-- Publications Begin -->
         <table width="100%" align="center" border="0" cellspacing="0" cellpadding="12">
@@ -197,97 +153,11 @@
           </colgroup>
           <tr>
             <td valign="center">
-                <strong>[USENIX Security'20]</strong>
+                <em>[2019]</em>
             </td>
             <td valign="center">
-                <papertitle>Measuring and Modeling the Label Dynamics of Online Anti-Malware Engines</papertitle>
-                <br>
-                Shuofei Zhu,
-                Jianjun Shi,
-                <strong><u>Limin Yang</u></strong>,
-                Boqin Qin, Ziyi Zhang, Linhai Song, Gang Wang
-                <br>
-                <a href="data/USENIX20_VirusTotal.pdf">PDF</a> /
-                <a href="https://sfzhu93.github.io/projects/vt/index.html">Data</a>
-            </td>
-          </tr>
-          <tr>
-            <td valign="center">
-                <em>[SafeThings'20]</em>
-            </td>
-            <td valign="center">
-                <papertitle>A Case Study of the Security Vetting Process of Smart-home Assistant Applications</papertitle>
-                <br>
-                Hang Hu,
-                <strong><u>Limin Yang</u></strong>,
-                Shihan Lin,
-                Gang Wang
-                <br>
-                <a href="data/SafeThings20_Alexa.pdf">PDF</a>
-            </td>
-          </tr>
-          <tr>
-            <td valign="center">
-                <strong>[IMC'19]</strong>
-            </td>
-            <td valign="center">
-                <papertitle>Opening the Blackbox of VirusTotal: Analyzing Online Phishing Scan Engines</papertitle>
-                <br>
-                Peng Peng,
-                <strong><u>Limin Yang</u></strong>,
-                Linhai Song,
-                Gang Wang
-                <br>
-                <a href="data/IMC19_VirusTotal.pdf">PDF</a> /
-                <a href="https://github.com/whyisyoung/virustotal">Data</a>
-            </td>
-          </tr>
-          <tr>
-            <td valign="center">
-                <strong>[USENIX Security'18]</strong>
-            </td>
-            <td valign="center">
-                <papertitle>Understanding the Reproducibility of Crowd-reported Security Vulnerabilities</papertitle>
-                <br>
-                Dongliang Mu,
-                Alejandro Cuevas,
-                <strong><u>Limin Yang</u></strong>,
-                Hang Hu,
-                Xinyu Xing,
-                Bing Mao,
-                Gang Wang
-                <br>
-                <a href="data/USENIX18_VulReproduction.pdf">PDF</a> /
-                <a href="https://vulnreproduction.github.io/">Data</a>
-            </td>
-          </tr>
-          <tr>
-            <td valign="center">
-                <em>[GLOBECOM'17]</em>
-            </td>
-            <td valign="center">
-                <papertitle>VulDigger: A Just-in-Time and Cost-Aware Tool for Digging Vulnerability-Contributing Changes</papertitle>
-                <br>
-                <strong><u>Limin Yang</u></strong>,
-                <!-- Xiangxue Li, Yu Yu -->
-                et al.
-                <br>
-                <a href="data/Globecom17_Vuldigger.pdf">PDF</a>
-            </td>
-          </tr>
-          <tr>
-            <td valign="center">
-                <em>[PPNA'17]</em>
-            </td>
-            <td valign="center">
-                <papertitle>Characterizing User Behaviors in Location-based Find-and-Flirt Services: Anonymity and Demographics</papertitle>
-                <br>
-                Minhui Xue,
-                <strong><u>Limin Yang</u></strong>,
-                Keith W. Ross,
-                Haifeng Qian
-                <br>
-                <a href="data/PPNA17_WeChat.pdf">PDF</a>
+                Xiu Xu, Haiyang Xue, Kunpeng Wang, Man Ho Au, Bei Liang, Song Tian <papertitle>Characterizing User Behaviors in Location-based Find-and-Flirt Services: Anonymity and Demographics</papertitle><br>
+                ASIACRYPT 2019 <a href="data/asia2019.pdf">Eprint</a>
             </td>
           </tr>
         </table>
@@ -304,46 +174,7 @@
 
         <!-- Preprints End -->
 
-        <!-- Internship Begin -->
-        <table width="100%" align="center" border="0" cellspacing="0" cellpadding="12">
-          <tr>
-            <td>
-              <heading>Internship</heading>
-            </td>
-          </tr>
-        </table>
-        <table width="100%" align="center" border="0" cellpadding="12" style="table-layout:fixed;">
-          <colgroup>
-                <col span="1" style="width: 23%;">
-                <col span="1" style="width: 77%;">
-          </colgroup>
-          <tr>
-            <td valign="center">
-              <a href="https://www.ist.psu.edu/"><papertitle>Penn State</papertitle></a>
-            </td>
-            <td valign="center">
-                <em>Research Intern</em>, Pennsylvania, US, 09/2017 – 02/2018
-            </td>
-          </tr>
-          <tr>
-            <td valign="center">
-                <a href="https://www.xueba100.com/"><papertitle>Xueba Jun</papertitle></a>
-            </td>
-            <td valign="center">
-              <em>Search and Rank Intern</em>, Shanghai, China, 09/2016 – 10/2016
-            </td>
-          </tr>
-          <tr>
-            <td valign="center">
-                <a href="http://english.pku.edu.cn/"><papertitle>Peking University</
-                  papertitle></a>
-            </td>
-            <td valign="center">
-              <em>Exploit Intern</em>, Beijing, China, 07/2015 – 08/2015
-            </td>
-          </tr>
-        </table>
-        <!-- Internship End -->
+      
 
         <!-- Awards Begin -->
         <table width="100%" align="center" border="0" cellspacing="0" cellpadding="12">
@@ -360,74 +191,16 @@
           </colgroup>
           <tr>
             <td valign="center">
-              2017
+              2020
             </td>
             <td>
-                <papertitle> ECNU Graduate Student Overseas Research Scholarship</papertitle>
-            </td>
-          </tr>
-          <tr>
-            <td valign="center">
-              2013-2015
-            </td>
-            <td>
-                <papertitle>ECNU Top-notch Innovative Personnel Training Plan</papertitle>
+                <papertitle>SIAKE: Second Price of National Cryptographic Algorithm Competition</papertitle>
             </td>
           </tr>
         </table>
         <!-- Awards End -->
 
-        <!-- Teaching Begin -->
-        <table width="100%" align="center" border="0" cellspacing="0" cellpadding="12">
-          <tr>
-            <td>
-              <heading>Teaching</heading>
-            </td>
-          </tr>
-        </table>
-        <table width="100%" align="center" border="0" cellpadding="12" style="table-layout:fixed;">
-          <colgroup>
-                <col span="1" style="width: 23%;">
-                <col span="1" style="width: 77%;">
-          </colgroup>
-          <tr>
-            <td valign="center">
-                2019 Spring
-            </td>
-            <td>
-                <papertitle>CS-4264 Principles of Computer Security</papertitle>,
-                Virginia Tech, Teaching Assistant
-            </td>
-          </tr>
-          <tr>
-            <td valign="center">
-              2018 Fall
-            </td>
-            <td>
-                <papertitle>CS-3114 Data Structures and Algorithms</papertitle>,
-                Virginia Tech, Teaching Assistant
-            </td>
-          </tr>
-        </table>
-        <!-- Teaching End -->
-
-        <table width="100%" align="center" border="0" cellpadding="12">
-          <tr>
-            <td width="100%" valign="center">
-              <p class="footer">
-                Design: <a href="https://jonbarron.info/" target="_blank" style="color: #777; font-size: 14px;">Jon Barron</a> <br>
-                Last updated:
-                <!-- <script>
-                  t = new Date(document.lastModified).toLocaleDateString()
-                  document.write(t);
-                </script> -->
-                <?php
-                  echo "".date("m/d/Y", filemtime("index.php"));
-                ?>
-              </p>
-            </td>
-          </tr>
-        </table>
+        
       </td>
     </tr>
   </table>
